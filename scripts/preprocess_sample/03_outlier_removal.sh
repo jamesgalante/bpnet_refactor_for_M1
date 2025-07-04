c3180ceaa331bdf3a8efcbf12259b86e0845b9f2
jamesgalante <james.a.galante@gmail.com>
2025-07-03 21:29:01 -0400

    Fixed bpnet command calling in processing script

--- scripts/preprocess_sample/03_outlier_removal.sh
+++ scripts/preprocess_sample/03_outlier_removal.sh
@@ -34,8 +34,7 @@ cat > input_outliers.json << EOF
 EOF
 
 echo "Running bpnet-outliers"
-export PYTHONPATH="../../bpnet-refactor:$PYTHONPATH"
-python -m bpnet.cli.bpnet_outliers \
+python ../../bpnet-refactor/bpnet/cli/outliers.py \
     --input-data input_outliers.json  \
     --quantile 0.99 \
     --quantile-value-scale-factor 1.2 \
